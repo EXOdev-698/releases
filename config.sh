15b1afdf5421fdf4cf270c891c80c1eea46f4a2f
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-10-10 18:21:12 +0500

    crDroid: olive build support

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="IAmDeadlylxrd"
+export GITHUB_EMAIL="IAmDeadlylxrd@gmail.com"
 
-export device=""
+export device="olive"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="crDroid"
+export ROM_DIR="${WORKSPACE}/../crDroidAndroid"
+export ROM_VERSION="7.11"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/IAmDeadlylxrd/local_manifests/master/crdroid-olive.xml"
+export manifest_url="https://github.com/crdroidandroid/android"
+export rom_vendor_name="lineage"
+export branch="11.0"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="250"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="IAmDeadlylxrd/releases"
 
 export timezone="UTC"
