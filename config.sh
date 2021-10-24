219ea01cbb6a84afec8a288a01b25fb860ed4dc6
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-10-24 14:22:45 +0500

    DotOS: juice builds support

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="IAmDeadlylxrd"
+export GITHUB_EMAIL="IAmDeadlylxrd@gmail.com"
 
-export device=""
+export device="juice"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="DotOS"
+export ROM_DIR="${WORKSPACE}/../Dot-11"
+export ROM_VERSION="5.2"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
-export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export local_manifest_url="https://raw.githubusercontent.com/IAmDeadlylxrd/local_manifests/master/dotos-juice.xml"
+export manifest_url="https://github.com/DotOS/manifest"
+export rom_vendor_name="dot"
+export branch="dot11"
+export bacon="bacon -j$(nproc --all)"
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="false"
+export ccache_size="0"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="IAmDeadlylxrd/releases"
 
 export timezone="UTC"
