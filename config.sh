7d6cfc54aa62cf3349d4fe1d0d5e017a7900bee6
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-11-01 15:06:53 +0000

    Sakura: Build sakura for olives

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="IAmDeadlylxrd"
+export GITHUB_EMAIL="IAmDeadlylxrd@gmail.com"
 
-export device=""
+export device="olives"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Project Sakura"
+export ROM_DIR="${WORKSPACE}/../Sakura-11"
+export ROM_VERSION="5.2"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
-export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export local_manifest_url="https://raw.githubusercontent.com/IAmDeadlylxrd/local_manifests/master/sakura-olives.xml"
+export manifest_url="https://github.com/ProjectSakura/android"
+export rom_vendor_name="lineage"
+export branch="11"
+export bacon="bacon -j$(nproc --all)"
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="150"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="IAmDeadlylxrd/releases"
 
 export timezone="UTC"
