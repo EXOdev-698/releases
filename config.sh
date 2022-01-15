f25c5d126d40fa92f1ded81da94900b34c3fb625
JonnyRoller_23 <71149028+JonnyRoller23@users.noreply.github.com>
2022-01-16 02:22:53 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ export GITHUB_EMAIL="maxim55miui11@gmail.com"
 export device="olives"
 
 export ROM="LineageOS-18.1"
-export ROM_DIR="${WORKSPACE}/../LineageOS-18.1-olives"
+export ROM_DIR="${WORKSPACE}/../LineageOS-Mods-18.1-olives"
 export ROM_VERSION="11"
 export official="false"
 export local_manifest_url="https://raw.githubusercontent.com/JonnyRoller23/manifest/main/lineage-18.1-olives.xml"
