1d3946f81e5b635fd24856b486f60cc40e97d2e6
JonnyRoller_23 <71149028+JonnyRoller23@users.noreply.github.com>
2022-01-26 00:08:00 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER="IAmDeadlylxrd"
-export GITHUB_EMAIL="IAmDeadlylxrd@gmail.com"
+export GITHUB_USER="JonnyRoller23"
+export GITHUB_EMAIL="maxim55miui11@gmail.com"
 
 export device="olives"
 
-export ROM="Project Radiant"
-export ROM_DIR="${WORKSPACE}/../ProjectRadiant-11"
-export ROM_VERSION="12.0.0"
+export ROM="CrDroid-12"
+export ROM_DIR="${WORKSPACE}/../CrDroid-olivelite-12"
+export ROM_VERSION="12.0"
 export official="false"
-export local_manifest_url="https://raw.githubusercontent.com/IAmDeadlylxrd/local_manifests/master/radiant-olives.xml"
-export manifest_url="https://github.com/ProjectRadiant/manifest"
-export rom_vendor_name="radiant"
-export branch="twelve"
+export local_manifest_url="https://raw.githubusercontent.com/JonnyRoller23/manifest/main/crdroid-v8-olivelite.xml"
+export manifest_url="https://github.com/crdroidandroid/android"
+export rom_vendor_name="crdroid"
+export branch="12.0"
 export bacon="bacon -j$(nproc --all)"
 export buildtype="userdebug"
 export clean="installclean"
 export generate_incremental="false"
 export upload_recovery="false"
 
-export ccache="false"
-export ccache_size="0"
+export ccache="true"
+export ccache_size="250"
 
 export jenkins="true"
 
-export release_repo="IAmDeadlylxrd/releases"
+export release_repo="JonnyRoller23/releases"
 
 export timezone="UTC"
