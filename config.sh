0a74cbd120b0e3a09a70b7041eb75861ba310bf5
JonnyRoller_23 <71149028+JonnyRoller23@users.noreply.github.com>
2022-01-25 21:39:15 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ export rom_vendor_name="radiant"
 export branch="twelve"
 export bacon="bacon"
 export buildtype="userdebug"
-export clean="installclean"
+export clean="clean"
 export generate_incremental="false"
 export upload_recovery="false"
 
