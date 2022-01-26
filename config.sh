06fda33583b5fc5efb17522d97d0220b8723d253
JonnyRoller_23 <71149028+JonnyRoller23@users.noreply.github.com>
2022-01-26 16:51:26 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER="IAmDeadlylxrd"
-export GITHUB_EMAIL="IAmDeadlylxrd@gmail.com"
+export GITHUB_USER="JonnyRoller23"
+export GITHUB_EMAIL="maxim55miui11@gmail.com"
 
 export device="olives"
 
-export ROM="Project Sakura"
-export ROM_DIR="${WORKSPACE}/../Sakura-11"
+export ROM="NusantaraProject-12"
+export ROM_DIR="${WORKSPACE}/../NusantaraProject-12-olivelite"
 export ROM_VERSION="5.2"
 export official="false"
-export local_manifest_url="https://raw.githubusercontent.com/IAmDeadlylxrd/local_manifests/master/sakura-olives.xml"
-export manifest_url="https://github.com/ProjectSakura/android"
-export rom_vendor_name="lineage"
-export branch="11"
-export bacon="bacon -j$(nproc --all)"
+export local_manifest_url="https://raw.githubusercontent.com/JonnyRoller23/manifest/main/nusantara-12-olivelite.xml"
+export manifest_url="https://github.com/NusantaraProject-ROM/android_manifest"
+export rom_vendor_name="nusantara"
+export branch="12"
+export bacon="bacon"
 export buildtype="userdebug"
 export clean="installclean"
 export generate_incremental="false"
 export upload_recovery="false"
 
 export ccache="true"
-export ccache_size="150"
+export ccache_size="250"
 
 export jenkins="true"
 
-export release_repo="IAmDeadlylxrd/releases"
+export release_repo="JonnyRoller23/releases"
 
 export timezone="UTC"
